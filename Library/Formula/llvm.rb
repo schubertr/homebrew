251671db7fe6d15baa7f99e18cf28d442c8cac8f
Ryan Schubert <schubertr@etown.edu>
2015-11-09 21:42:14 -0500

    llvm.rb now supports 3.7.0

--- Library/Formula/llvm.rb
+++ Library/Formula/llvm.rb
@@ -17,57 +17,74 @@ class CodesignRequirement < Requirement
   end
 end
 
-class Llvm < Formula
-  desc "llvm (Low Level Virtual Machine): a next-gen compiler infrastructure"
+class Llvm37 < Formula
+  desc "The LLVM Compiler Infrastructure"
   homepage "http://llvm.org/"
 
   stable do
-    url "http://llvm.org/releases/3.6.2/llvm-3.6.2.src.tar.xz"
-    sha256 "f60dc158bfda6822de167e87275848969f0558b3134892ff54fced87e4667b94"
+    url "http://llvm.org/releases/3.7.0/llvm-3.7.0.src.tar.xz"
+    sha256 "ab45895f9dcdad1e140a3a79fd709f64b05ad7364e308c0e582c5b02e9cc3153"
 
     resource "clang" do
-      url "http://llvm.org/releases/3.6.2/cfe-3.6.2.src.tar.xz"
-      sha256 "ae9180466a23acb426d12444d866b266ff2289b266064d362462e44f8d4699f3"
-    end
-
-    resource "libcxx" do
-      url "http://llvm.org/releases/3.6.2/libcxx-3.6.2.src.tar.xz"
-      sha256 "52f3d452f48209c9df1792158fdbd7f3e98ed9bca8ebb51fcd524f67437c8b81"
-    end
-
-    resource "lld" do
-      url "http://llvm.org/releases/3.6.2/lld-3.6.2.src.tar.xz"
-      sha256 "43f553c115563600577764262f1f2fac3740f0c639750f81e125963c90030b33"
-    end
-
-    resource "lldb" do
-      url "http://llvm.org/releases/3.6.2/lldb-3.6.2.src.tar.xz"
-      sha256 "940dc96b64919b7dbf32c37e0e1d1fc88cc18e1d4b3acf1e7dfe5a46eb6523a9"
+      url "http://llvm.org/releases/3.7.0/cfe-3.7.0.src.tar.xz"
+      sha256 "4ed740c5a91df1c90a4118c5154851d6a475f39a91346bdf268c1c29c13aa1cc"
     end
 
     resource "clang-tools-extra" do
-      url "http://llvm.org/releases/3.6.2/clang-tools-extra-3.6.2.src.tar.xz"
-      sha256 "6a0ec627d398f501ddf347060f7a2ccea4802b2494f1d4fd7bda3e0442d04feb"
+      url "http://llvm.org/releases/3.7.0/clang-tools-extra-3.7.0.src.tar.xz"
+      sha256 "8ae8a0a3a96b7a700412d67df0af172cb2fc1326beec575fcc0f71d2e72709cd"
     end
-  end
 
-  bottle do
-    cellar :any
-    sha256 "fa04afc62800a236e32880efe30e1dbb61eace1e7e9ec20d2d53393ef9d68636" => :el_capitan
-    sha256 "a0ec4b17ae8c1c61071e603d0dcf3e1c39a5aae63c3f8237b4363a06701a3319" => :yosemite
-    sha256 "17a62c19d119c88972fa3dce920cfbc6150af8892ba8e29ce551ae7e2e84f42e" => :mavericks
-    sha256 "6d780faae2647ebce704b2f0a246b52d4037ebf4a2f796644814607e7751af93" => :mountain_lion
-  end
+    resource "compiler-rt" do
+      url "http://llvm.org/releases/3.7.0/compiler-rt-3.7.0.src.tar.xz"
+      sha256 "227fa998520bc94974a428dc8e7654d9bdf277e5bc70d4064ebc05691bd62b0b"
+    end
 
-  head do
-    url "http://llvm.org/git/llvm.git"
+    resource "polly" do
+      url "http://llvm.org/releases/3.7.0/polly-3.7.0.src.tar.xz"
+      sha256 "3e5f3f4dc141c7d25b36b910d48c7da74ecc92f10cea5b568c909623d6067edf"
+    end
 
-    resource "clang" do
-      url "http://llvm.org/git/clang.git"
+    resource "lld" do
+      url "http://llvm.org/releases/3.7.0/lld-3.7.0.src.tar.xz"
+      sha256 "ddb658b789c501efbe4f54ff8ced2c07cd9ff686c92445d8a1ab2cd5dbd837ed"
+    end
+
+    resource "lldb" do
+      url "http://llvm.org/releases/3.7.0/lldb-3.7.0.src.tar.xz"
+      sha256 "f4d7505bc111044eaa4033af012221e492938405b62522b8e3e354c20c4b71e9"
     end
 
     resource "libcxx" do
-      url "http://llvm.org/git/libcxx.git"
+      url "http://llvm.org/releases/3.7.0/libcxx-3.7.0.src.tar.xz"
+      sha256 "c18f3c8333cd7e678c1424a57fe5e25efe740ca7caf62ac67152b4723f3ad08e"
+    end
+
+    if MacOS.version <= :snow_leopard
+      resource "libcxxabi" do
+        url "http://llvm.org/releases/3.7.0/libcxxabi-3.7.0.src.tar.xz"
+        sha256 "48b074fd334958b2d8bab893c897a0c8258328782cdec2d229c7bce432b49beb"
+      end
+    end
+  end
+
+  head do
+    url "http://llvm.org/git/llvm.git", :branch => "release_37"
+
+    resource "clang" do
+      url "http://llvm.org/git/clang.git", :branch => "release_37"
+    end
+
+    resource "clang-tools-extra" do
+      url "http://llvm.org/git/clang-tools-extra.git", :branch => "release_37"
+    end
+
+    resource "compiler-rt" do
+      url "http://llvm.org/git/compiler-rt.git", :branch => "release_37"
+    end
+
+    resource "polly" do
+      url "http://llvm.org/git/polly.git", :branch => "release_37"
     end
 
     resource "lld" do
@@ -75,38 +92,47 @@ class Llvm < Formula
     end
 
     resource "lldb" do
-      url "http://llvm.org/git/lldb.git"
+      url "http://llvm.org/git/lldb.git", :branch => "release_37"
     end
 
-    resource "clang-tools-extra" do
-      url "http://llvm.org/git/clang-tools-extra.git"
+    resource "libcxx" do
+      url "http://llvm.org/git/libcxx.git", :branch => "release_37"
+    end
+
+    if MacOS.version <= :snow_leopard
+      resource "libcxxabi" do
+        url "http://llvm.org/git/libcxxabi.git", :branch => "release_37"
+      end
     end
   end
+
+  patch :DATA
 
   option :universal
-  option "with-clang", "Build Clang support library"
   option "with-lld", "Build LLD linker"
   option "with-lldb", "Build LLDB debugger"
-  option "with-rtti", "Build with C++ RTTI"
-  option "with-python", "Build Python bindings against Homebrew Python"
+  option "with-asan", "Include support for -faddress-sanitizer (from compiler-rt)"
+  option "with-all-targets", "Build all target backends"
+  option "with-python", "Build lldb bindings against the python in PATH instead of system Python"
+  option "without-shared", "Don't build LLVM as a shared library"
   option "without-assertions", "Speeds up LLVM, but provides less debug information"
 
-  deprecated_option "rtti" => "with-rtti"
-  deprecated_option "disable-assertions" => "without-assertions"
-
-  if MacOS.version <= :snow_leopard
-    depends_on :python
-  else
-    depends_on :python => :optional
-  end
-  depends_on "cmake" => :build
+  depends_on "gmp"
+  depends_on "libffi" => :recommended
+  depends_on :python => :optional
 
   if build.with? "lldb"
     depends_on "swig"
     depends_on CodesignRequirement
   end
 
-  keg_only :provided_by_osx
+  # version suffix
+  def ver
+    "3.7"
+  end
+
+  # LLVM installs its own standard library which confuses stdlib checking.
+  cxxstdlib_check :skip
 
   # Apple's libstdc++ is too old to build LLVM
   fails_with :gcc
@@ -116,80 +142,147 @@ class Llvm < Formula
     # Apple's libstdc++ is too old to build LLVM
     ENV.libcxx if ENV.compiler == :clang
 
-    if build.with?("lldb") && build.without?("clang")
-      raise "Building LLDB needs Clang support library."
-    end
+    clang_buildpath = buildpath/"tools/clang"
+    libcxx_buildpath = buildpath/"projects/libcxx"
+    libcxxabi_buildpath = buildpath/"libcxxabi" # build failure if put in projects due to no Makefile
 
-    if build.with? "clang"
-      (buildpath/"projects/libcxx").install resource("libcxx")
-      (buildpath/"tools/clang").install resource("clang")
-      (buildpath/"tools/clang/tools/extra").install resource("clang-tools-extra")
-    end
-
+    clang_buildpath.install resource("clang")
+    libcxx_buildpath.install resource("libcxx")
+    (buildpath/"tools/polly").install resource("polly")
+    (buildpath/"tools/clang/tools/extra").install resource("clang-tools-extra")
     (buildpath/"tools/lld").install resource("lld") if build.with? "lld"
     (buildpath/"tools/lldb").install resource("lldb") if build.with? "lldb"
-
-    args = %w[
-      -DLLVM_OPTIMIZED_TABLEGEN=On
-    ]
-
-    args << "-DLLVM_ENABLE_RTTI=On" if build.with? "rtti"
-
-    if build.with? "assertions"
-      args << "-DLLVM_ENABLE_ASSERTIONS=On"
-    else
-      args << "-DCMAKE_CXX_FLAGS_RELEASE='-DNDEBUG'"
-    end
+    (buildpath/"projects/compiler-rt").install resource("compiler-rt") if build.with? "asan"
 
     if build.universal?
       ENV.permit_arch_flags
-      args << "-DCMAKE_OSX_ARCHITECTURES=#{Hardware::CPU.universal_archs.as_cmake_arch_flags}"
+      ENV["UNIVERSAL"] = "1"
+      ENV["UNIVERSAL_ARCH"] = Hardware::CPU.universal_archs.join(" ")
     end
+
+    ENV["REQUIRES_RTTI"] = "1"
+
+    install_prefix = lib/"llvm-#{ver}"
+
+    args = [
+      "--prefix=#{install_prefix}",
+      "--enable-optimized",
+      "--disable-bindings",
+      "--with-gmp=#{Formula["gmp"].opt_prefix}",
+    ]
+
+    if build.with? "all-targets"
+      args << "--enable-targets=all"
+    else
+      args << "--enable-targets=host"
+    end
+
+    args << "--enable-shared" if build.with? "shared"
+    args << "--disable-assertions" if build.without? "assertions"
+    args << "--enable-libffi" if build.with? "libffi"
 
     mktemp do
-      system "cmake", "-G", "Unix Makefiles", buildpath, *(std_cmake_args + args)
-      system "make"
-      system "make", "install"
+      system buildpath/"configure", *args
+      system "make", "VERBOSE=1"
+      system "make", "VERBOSE=1", "install"
     end
 
-    if build.with? "clang"
-      system "make", "-C", "projects/libcxx", "install",
-        "DSTROOT=#{prefix}", "SYMROOT=#{buildpath}/projects/libcxx"
+    if MacOS.version <= :snow_leopard
+      libcxxabi_buildpath.install resource("libcxxabi")
 
-      (share/"clang/tools").install Dir["tools/clang/tools/scan-{build,view}"]
-      inreplace "#{share}/clang/tools/scan-build/scan-build", "$RealBin/bin/clang", "#{bin}/clang"
-      bin.install_symlink share/"clang/tools/scan-build/scan-build", share/"clang/tools/scan-view/scan-view"
-      man1.install_symlink share/"clang/tools/scan-build/scan-build.1"
+      cd libcxxabi_buildpath/"lib" do
+        # Set rpath to save user from setting DYLD_LIBRARY_PATH
+        inreplace "buildit", "-install_name /usr/lib/libc++abi.dylib", "-install_name #{install_prefix}/usr/lib/libc++abi.dylib"
+
+        ENV["CC"] = "#{install_prefix}/bin/clang"
+        ENV["CXX"] = "#{install_prefix}/bin/clang++"
+        ENV["TRIPLE"] = "*-apple-*"
+        system "./buildit"
+        (install_prefix/"usr/lib").install "libc++abi.dylib"
+        cp libcxxabi_buildpath/"include/cxxabi.h", install_prefix/"lib/c++/v1"
+      end
+
+      # Snow Leopard make rules hardcode libc++ and libc++abi path.
+      # Change to Cellar path here.
+      inreplace "#{libcxx_buildpath}/lib/buildit" do |s|
+        s.gsub! "-install_name /usr/lib/libc++.1.dylib", "-install_name #{install_prefix}/usr/lib/libc++.1.dylib"
+        s.gsub! "-Wl,-reexport_library,/usr/lib/libc++abi.dylib", "-Wl,-reexport_library,#{install_prefix}/usr/lib/libc++abi.dylib"
+      end
+
+      # On Snow Leopard and older system libc++abi is not shipped but
+      # needed here. It is hard to tweak environment settings to change
+      # include path as libc++ uses a custom build script, so just
+      # symlink the needed header here.
+      ln_s libcxxabi_buildpath/"include/cxxabi.h", libcxx_buildpath/"include"
     end
 
-    # install llvm python bindings
-    (lib/"python2.7/site-packages").install buildpath/"bindings/python/llvm"
-    (lib/"python2.7/site-packages").install buildpath/"tools/clang/bindings/python/clang" if build.with? "clang"
+    # Putting libcxx in projects only ensures that headers are installed.
+    # Manually "make install" to actually install the shared libs.
+    libcxx_make_args = [
+      # Use the built clang for building
+      "CC=#{install_prefix}/bin/clang",
+      "CXX=#{install_prefix}/bin/clang++",
+      # Properly set deployment target, which is needed for Snow Leopard
+      "MACOSX_DEPLOYMENT_TARGET=#{MacOS.version}",
+      # The following flags are needed so it can be installed correctly.
+      "DSTROOT=#{install_prefix}",
+      "SYMROOT=#{libcxx_buildpath}",
+    ]
+
+    system "make", "-C", libcxx_buildpath, "install", *libcxx_make_args
+
+    (share/"clang-#{ver}/tools").install Dir["tools/clang/tools/scan-{build,view}"]
+    inreplace share/"clang-#{ver}/tools/scan-build/scan-build", "$RealBin/bin/clang", install_prefix/"bin/clang"
+    ln_s share/"clang-#{ver}/tools/scan-build/scan-build", install_prefix/"bin"
+    ln_s share/"clang-#{ver}/tools/scan-view/scan-view", install_prefix/"bin"
+    (install_prefix/"share/man/man1").install share/"clang-#{ver}/tools/scan-build/scan-build.1"
+
+    (lib/"python2.7/site-packages").install "bindings/python/llvm" => "llvm-#{ver}",
+                                            clang_buildpath/"bindings/python/clang" => "clang-#{ver}"
+    (lib/"python2.7/site-packages").install_symlink install_prefix/"lib/python2.7/site-packages/lldb" => "lldb-#{ver}" if build.with? "lldb"
+
+    Dir.glob(install_prefix/"bin/*") do |exec_path|
+      basename = File.basename(exec_path)
+      bin.install_symlink exec_path => "#{basename}-#{ver}"
+    end
+
+    Dir.glob(install_prefix/"share/man/man1/*") do |manpage|
+      basename = File.basename(manpage, ".1")
+      man1.install_symlink manpage => "#{basename}-#{ver}.1"
+    end
   end
 
-  def caveats
-    <<-EOS.undent
-      LLVM executables are installed in #{opt_bin}.
-      Extra tools are installed in #{opt_share}/llvm.
+  def caveats; <<-EOS.undent
+    Extra tools are installed in #{opt_share}/clang-#{ver}
+
+    To link to libc++, something like the following is required:
+      CXX="clang++-#{ver} -stdlib=libc++"
+      CXXFLAGS="$CXXFLAGS -nostdinc++ -I#{opt_lib}/llvm-#{ver}/include/c++/v1"
+      LDFLAGS="$LDFLAGS -L#{opt_lib}/llvm-#{ver}/lib"
     EOS
   end
 
   test do
-    assert_equal prefix.to_s, shell_output("#{bin}/llvm-config --prefix").chomp
-
-    if build.with? "clang"
-      (testpath/"test.cpp").write <<-EOS.undent
-        #include <iostream>
-        using namespace std;
-
-        int main()
-        {
-          cout << "Hello World!" << endl;
-          return 0;
-        }
-      EOS
-      system "#{bin}/clang++", "test.cpp", "-o", "test"
-      system "./test"
-    end
+    system "#{bin}/llvm-config-#{ver}", "--version"
   end
 end
+
+__END__
+diff --git a/Makefile.rules b/Makefile.rules
+index ebebc0a..b0bb378 100644
+--- a/Makefile.rules
++++ b/Makefile.rules
+@@ -599,7 +599,12 @@ ifneq ($(HOST_OS), $(filter $(HOST_OS), Cygwin MingW))
+ ifneq ($(HOST_OS),Darwin)
+   LD.Flags += $(RPATH) -Wl,'$$ORIGIN'
+ else
+-  LD.Flags += -Wl,-install_name  -Wl,"@rpath/lib$(LIBRARYNAME)$(SHLIBEXT)"
++  LD.Flags += -Wl,-install_name
++  ifdef LOADABLE_MODULE
++    LD.Flags += -Wl,"$(PROJ_libdir)/$(LIBRARYNAME)$(SHLIBEXT)"
++  else
++    LD.Flags += -Wl,"$(PROJ_libdir)/$(SharedPrefix)$(LIBRARYNAME)$(SHLIBEXT)"
++  endif
+ endif
+ endif
+ endif
